cc203bc59056da370d6eef823f4ff521735c4cc4
CodePapayas <catspianos@gmail.com>
2024-06-20 00:14:25 -0700

    Open CORS for testing

--- wilkinza-portfolio/backend/grocery-tracker-controller.mjs
+++ wilkinza-portfolio/backend/grocery-tracker-controller.mjs
@@ -17,14 +17,7 @@ const app = express();
 const allowedOrigins = ['https://portfoliorepo.onrender.com', 'http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:8000', 'http://localhost:10000'];
 
 app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type'
 }));
